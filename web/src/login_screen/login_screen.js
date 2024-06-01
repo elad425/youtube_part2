b1159a6fee7a0ecc6b94079fd88994017520fb8c
elad425 <elad425@gmail.com>
2024-06-02 01:54:39 +0300

    add signup screen and user validation

--- web/src/login_screen/login_screen.js
+++ web/src/login_screen/login_screen.js
@@ -8,8 +8,8 @@ const Verify_user = () => {
   const forms = document.querySelectorAll('.needs-validation')
 
   forms.forEach(form => {
-    form.classList.remove('is-valid')
-    form.classList.add('is-invalid')
+    form.classNameList.remove('is-valid')
+    form.classNameList.add('is-invalid')
   })
 
   var i = 0
@@ -17,8 +17,8 @@ const Verify_user = () => {
     if (user_data[i].email === email) {
       if (user_data[i].password === password) {
         forms.forEach(form => {
-          form.classList.remove('is-invalid')
-          form.classList.add('is-valid')
+          form.classNameList.remove('is-invalid')
+          form.classNameList.add('is-valid')
         })
       }
     }
@@ -29,33 +29,33 @@ const Verify_user = () => {
 function login_screen() {
   return (
     <body>
-      <div class="container d-flex justify-content-center align-items-center vh-100">
-        <div class="row border rounded-1 p-3 bg-light shadow box-area">
-          <div class="col-md-6 d-flex justify-content-center align-items-center flex-column left-box">
-            <div class="featured-image mb-3">
-              <img src="youtube.png" class="img-fluid p-3" alt="Responsive image"></img>
+      <div className="container d-flex justify-content-center align-items-center vh-100">
+        <div className="row border rounded-1 p-3 bg-light shadow box-area">
+          <div className="col-md-6 d-flex justify-content-center align-items-center flex-column left-box">
+            <div className="featured-image mb-3">
+              <img src="youtube.png" className="img-fluid p-3" alt="Responsive image"></img>
             </div>
           </div>
-          <div class="col-md-6 right-box">
-            <div class="row align-items-center">
-              <div class="header-text mb-4">
+          <div className="col-md-6 right-box">
+            <div className="row align-items-center">
+              <div className="header-text mb-4">
                 <h1>Hello</h1>
                 <t>please varify yourself</t>
               </div>
-              <div class="input-group mb-3">
-                <input type="text" class="form-control needs-validation form-control-lg bg-light fs-6" id="email" placeholder="Email address" ></input>
+              <div className="input-group mb-3">
+                <input type="text" className="form-control needs-validation form-control-lg bg-light fs-6" id="email" placeholder="Email address" ></input>
               </div>
-              <div class="input-group mb-1">
-                <input type="password" class="form-control needs-validation form-control-lg bg-light fs-6" id="password" placeholder="Password" ></input>
-                <div class="invalid-feedback">
+              <div className="input-group mb-1">
+                <input type="password" className="form-control needs-validation form-control-lg bg-light fs-6" id="password" placeholder="Password" ></input>
+                <div className="invalid-feedback">
                   wrong email or password.
                 </div>
               </div>
-              <div class="input-group mb-3 pt-5">
-                <button class="btn btn-lg btn-primary w-100" onClick={Verify_user} type='submit'>Login</button>
+              <div className="input-group mb-3 pt-5">
+                <button className="btn btn-lg btn-primary w-100" onClick={Verify_user} type='submit'>Login</button>
               </div>
-              <div class="row">
-                <div class="col">
+              <div className="row">
+                <div className="col">
                   <small>Don't have account? <a href="#">Sign Up</a></small>
                 </div>
               </div>
--- web/src/signup_screen/signup_screen.js
+++ web/src/signup_screen/signup_screen.js
@@ -1,5 +1,6 @@
 import './signup_screen.css';
 import user_data from '../user_info.json';
+import Signup_input from '../components/signup_input/signup_input';
 
 const signup_user = () => {
     var email = document.getElementById("email").value;
@@ -9,36 +10,36 @@ const signup_user = () => {
     const regex = /^(?=.*[A-Za-z])(?=.*\d)/;
     var valid = 1
 
-    if (user_name === ''){
-        document.getElementById('user_name').classList.add('is-invalid')
+    if (user_name === '') {
+        document.getElementById('user_name').classNameList.add('is-invalid')
         valid = 0
     } else {
-        document.getElementById('user_name').classList.remove('is-invalid')
+        document.getElementById('user_name').classNameList.remove('is-invalid')
     }
 
     // check if email already exists
     for (const user of user_data) {
         if (user.email === email || email === '') {
-            document.getElementById('email').classList.add('is-invalid')
+            document.getElementById('email').classNameList.add('is-invalid')
             valid = 0
             break
         } else {
-            document.getElementById('email').classList.remove('is-invalid')
+            document.getElementById('email').classNameList.remove('is-invalid')
         }
     }
 
     // check if password contain 8 characters with letters and numbers
     if (!regex.test(password) || password.length < 8) {
-        document.getElementById('password').classList.add('is-invalid')
+        document.getElementById('password').classNameList.add('is-invalid')
         valid = 0
     } else {
-        document.getElementById('password').classList.remove('is-invalid')
+        document.getElementById('password').classNameList.remove('is-invalid')
         // checks if both passwords are equal
         if (password != con_password) {
-            document.getElementById('con_password').classList.add('is-invalid')
+            document.getElementById('con_password').classNameList.add('is-invalid')
             valid = 0
         } else {
-            document.getElementById('con_password').classList.remove('is-invalid')
+            document.getElementById('con_password').classNameList.remove('is-invalid')
         }
     }
 
@@ -51,8 +52,8 @@ const signup_user = () => {
             password: password
         }
         forms.forEach(form => {
-            form.classList.remove('is-invalid')
-            form.classList.add('is-valid')
+            form.classNameList.remove('is-invalid')
+            form.classNameList.add('is-valid')
         })
         user_data.push(data)
     }
@@ -60,53 +61,30 @@ const signup_user = () => {
 
 function Signup_screen() {
     return (
-        <body>
-            <div class="container d-flex justify-content-center align-items-center vh-100">
-                <div class="row border rounded-1 p-3 bg-light shadow box-area">
-                    <div class="col-md-6 d-flex justify-content-center align-items-center flex-column left-box">
-                        <div class="featured-image mb-3">
-                            <img src="youtube.png" class="img-fluid p-3" alt="Responsive image"></img>
-                        </div>
+        <div className="container d-flex justify-content-center align-items-center vh-100">
+            <div className="row border rounded-1 p-3 bg-light shadow box-area">
+                <div className="col-md-6 d-flex justify-content-center align-items-center flex-column left-box">
+                    <div className="featured-image mb-3">
+                        <img src="youtube.png" className="img-fluid p-3" alt="Responsive image"></img>
                     </div>
-                    <div class="col-md-6 right-box">
-                        <div class="row align-items-center">
-                            <div class="header-text mb-4">
-                                <h1>Sign up!</h1>
-                                <t>please fill the required fields</t>
-                            </div>
-                            <div class="input-group mb-3">
-                                <input type="text" class="form-control needs-validation form-control-lg bg-light fs-6" id="user_name" placeholder="User name" ></input>
-                                <div class="invalid-feedback">
-                                    please enter user name
-                                </div>
-                            </div>
-                            <div class="input-group mb-3">
-                                <input type="text" class="form-control needs-validation form-control-lg bg-light fs-6" id="email" placeholder="Email address" ></input>
-                                <div class="invalid-feedback">
-                                    invalid email adress or email already exists
-                                </div>
-                            </div>
-                            <div class="input-group mb-3">
-                                <input type="password" class="form-control needs-validation form-control-lg bg-light fs-6" id="password" placeholder="Password" ></input>
-                                <div class="invalid-feedback">
-                                    password need to contain at least 8 characters with letters and numbers
-                                </div>
-                            </div>
-                            <div class="input-group mb-5">
-                                <input type="password" class="form-control needs-validation form-control-lg bg-light fs-6" id="con_password" placeholder="cofirm password" ></input>
-                                <div class="invalid-feedback">
-                                    password doesnt match
-                                </div>
-                            </div>
-
-                            <div class="input-group mb-3">
-                                <button class="btn btn-lg btn-primary w-100" onClick={signup_user} type='submit'>Sign up</button>
-                            </div>
+                </div>
+                <div className="col-md-6 right-box">
+                    <div className="row align-items-center">
+                        <div className="header-text mb-4">
+                            <h1>Sign up!</h1>
+                            <div>please fill the required fields</div>
+                        </div>
+                        <Signup_input id="user_name" placeholder="user name" invalid="please enter user name" type="text" />
+                        <Signup_input id="email" placeholder="Email adress" invalid="invalid email adress or email already exists" type="text" />
+                        <Signup_input id="password" placeholder="Password" invalid="password need to contain at least 8 characters with letters and numbers" type="password" />
+                        <Signup_input id="con_password" placeholder="Password" invalid="password doesnt match" type="password" />
+                        <div className="input-group mb-3">
+                            <button className="btn btn-lg btn-primary w-100" onClick={signup_user} type='submit'>Sign up</button>
                         </div>
                     </div>
                 </div>
             </div>
-        </body>
+        </div>
     );
 }
 

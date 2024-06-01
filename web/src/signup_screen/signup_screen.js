de250996b09def16a0e70803b12ceb087d563dbf
elad425 <elad425@gmail.com>
2024-06-02 02:00:19 +0300

    add signup screen and user valitation

--- web/src/signup_screen/signup_screen.js
+++ web/src/signup_screen/signup_screen.js
@@ -11,35 +11,35 @@ const signup_user = () => {
     var valid = 1
 
     if (user_name === '') {
-        document.getElementById('user_name').classNameList.add('is-invalid')
+        document.getElementById('user_name').classList.add('is-invalid')
         valid = 0
     } else {
-        document.getElementById('user_name').classNameList.remove('is-invalid')
+        document.getElementById('user_name').classList.remove('is-invalid')
     }
 
     // check if email already exists
     for (const user of user_data) {
         if (user.email === email || email === '') {
-            document.getElementById('email').classNameList.add('is-invalid')
+            document.getElementById('email').classList.add('is-invalid')
             valid = 0
             break
         } else {
-            document.getElementById('email').classNameList.remove('is-invalid')
+            document.getElementById('email').classList.remove('is-invalid')
         }
     }
 
     // check if password contain 8 characters with letters and numbers
     if (!regex.test(password) || password.length < 8) {
-        document.getElementById('password').classNameList.add('is-invalid')
+        document.getElementById('password').classList.add('is-invalid')
         valid = 0
     } else {
-        document.getElementById('password').classNameList.remove('is-invalid')
+        document.getElementById('password').classList.remove('is-invalid')
         // checks if both passwords are equal
         if (password != con_password) {
-            document.getElementById('con_password').classNameList.add('is-invalid')
+            document.getElementById('con_password').classList.add('is-invalid')
             valid = 0
         } else {
-            document.getElementById('con_password').classNameList.remove('is-invalid')
+            document.getElementById('con_password').classList.remove('is-invalid')
         }
     }
 
@@ -52,8 +52,8 @@ const signup_user = () => {
             password: password
         }
         forms.forEach(form => {
-            form.classNameList.remove('is-invalid')
-            form.classNameList.add('is-valid')
+            form.classList.remove('is-invalid')
+            form.classList.add('is-valid')
         })
         user_data.push(data)
     }
@@ -61,30 +61,34 @@ const signup_user = () => {
 
 function Signup_screen() {
     return (
-        <div className="container d-flex justify-content-center align-items-center vh-100">
-            <div className="row border rounded-1 p-3 bg-light shadow box-area">
-                <div className="col-md-6 d-flex justify-content-center align-items-center flex-column left-box">
-                    <div className="featured-image mb-3">
-                        <img src="youtube.png" className="img-fluid p-3" alt="Responsive image"></img>
-                    </div>
-                </div>
-                <div className="col-md-6 right-box">
-                    <div className="row align-items-center">
-                        <div className="header-text mb-4">
-                            <h1>Sign up!</h1>
-                            <div>please fill the required fields</div>
+        <body>
+            <div class="container d-flex justify-content-center align-items-center vh-100">
+                <div class="row border rounded-1 p-3 bg-light shadow box-area">
+                    <div class="col-md-6 d-flex justify-content-center align-items-center flex-column left-box">
+                        <div class="featured-image mb-3">
+                            <img src="youtube.png" class="img-fluid p-3" alt="Responsive image"></img>
                         </div>
-                        <Signup_input id="user_name" placeholder="user name" invalid="please enter user name" type="text" />
-                        <Signup_input id="email" placeholder="Email adress" invalid="invalid email adress or email already exists" type="text" />
-                        <Signup_input id="password" placeholder="Password" invalid="password need to contain at least 8 characters with letters and numbers" type="password" />
-                        <Signup_input id="con_password" placeholder="Password" invalid="password doesnt match" type="password" />
-                        <div className="input-group mb-3">
-                            <button className="btn btn-lg btn-primary w-100" onClick={signup_user} type='submit'>Sign up</button>
+                    </div>
+                    <div class="col-md-6 right-box">
+                        <div class="row align-items-center">
+                            <div class="header-text mb-4">
+                                <h1>Sign up!</h1>
+                                <t>please fill the required fields</t>
+                            </div>
+
+                            <Signup_input id="user_name" placeholder="user name" invalid="please enter user name" type="text" />
+                            <Signup_input id="email" placeholder="Email adress" invalid="invalid email adress or email already exists" type="text" />
+                            <Signup_input id="password" placeholder="Password" invalid="password need to contain at least 8 characters with letters and numbers" type="password" />
+                            <Signup_input id="con_password" placeholder="Password" invalid="password doesnt match" type="password" />
+
+                            <div class="input-group mb-3">
+                                <button class="btn btn-lg btn-primary w-100" onClick={signup_user} type='submit'>Sign up</button>
+                            </div>
                         </div>
                     </div>
                 </div>
             </div>
-        </div>
+        </body>
     );
 }
 
